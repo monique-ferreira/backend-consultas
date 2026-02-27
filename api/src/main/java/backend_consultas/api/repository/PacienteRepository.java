7d76079ab796ac0d0a30bf7a2869a9fbcf14f308
MoniqueFIAP <anjos.moniqueferreira@gmail.com>
2026-02-27 09:39:42 -0300

    add: Paciente Repository

---
+++ api/src/main/java/backend_consultas/api/repository/PacienteRepository.java
@@ -0,0 +1,6 @@
+package backend_consultas.api.repository;
+import backend_consultas.api.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
