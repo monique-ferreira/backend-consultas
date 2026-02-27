4cee9b67175410ed9ed297fa82a60cff944ea721
MoniqueFIAP <anjos.moniqueferreira@gmail.com>
2026-02-27 08:54:54 -0300

    add: criação da entidade Especialidade

---
+++ api/src/main/java/backend_consultas/api/model/Especialidade.java
@@ -0,0 +1,25 @@
+package backend_consultas.api.model;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "especialidades")
+
+public class Especialidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    private String descricao;
+    public Especialidade() {}
+    public Especialidade(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() { return id; }
+    public String getNome() { return nome; }
+    public String getDescricao() { return descricao; }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+}
