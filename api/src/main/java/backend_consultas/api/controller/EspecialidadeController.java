f736bac4dd5e15b4e8299a51cfc2ac2731dc4141
MoniqueFIAP <anjos.moniqueferreira@gmail.com>
2026-02-27 09:09:13 -0300

    add: criação do controller de Especialidade

---
+++ api/src/main/java/backend_consultas/api/controller/EspecialidadeController.java
@@ -0,0 +1,24 @@
+package backend_consultas.api.controller;
+import backend_consultas.api.model.Especialidade;
+import backend_consultas.api.repository.EspecialidadeRepository;
+import backend_consultas.api.service.EspecialidadeService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping
+@CrossOrigin
+
+public class EspecialidadeController {
+    private final EspecialidadeService service;
+    public EspecialidadeController(EspecialidadeService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public Especialidade criar(@RequestBody Especialidade especialidade) {
+        return service.salvar(especialidade);
+    }
+    @GetMapping
+    public List<Especialidade> listar() {
+        return service.listar();
+    }
+}
