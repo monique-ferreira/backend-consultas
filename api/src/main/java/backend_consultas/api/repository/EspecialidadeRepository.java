c04c4fdb09420fd8b4816960c3154429e4cb3cfa
MoniqueFIAP <anjos.moniqueferreira@gmail.com>
2026-02-27 08:59:40 -0300

    add: criaçao do repository de Especialidade

---
+++ api/src/main/java/backend_consultas/api/repository/EspecialidadeRepository.java
@@ -0,0 +1,7 @@
+package backend_consultas.api.repository;
+import backend_consultas.api.model.Especialidade;
+import backend_consultas.api.model.Especialidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+}
